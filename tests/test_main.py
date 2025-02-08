62149d2f4769bd0d4c1421c11afe1c72507c5750
Garo Balouzian <45246707+GaroBal@users.noreply.github.com>
2025-02-08 16:34:39 -0500

    Project Setup (#1)
    * CircleCI Commit
    
    * Initial Project Set-Up
    
    * update config.yml to include a test job
    
    * fix lint issues
    
    * exclude venv when running flake8 checks
    
    ---------
    
    Co-authored-by: Garo Balouzian <garobalouzian@gmail.com>

---
+++ tests/test_main.py
@@ -0,0 +1,15 @@
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_get():
+    # Send a GET request to the root endpoint
+    response = client.get("/ping")
+
+    assert response.status_code == 200
+
+    # Assert the response JSON matches the expected output
+    assert response.json() == ["pong"]
